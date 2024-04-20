7ffca1a4bec170a4f88eb4f76fe51c7d8a7c3add
LLlE0 <glider.coolhacker@gmail.com>
2024-04-20 08:48:36 +0300

    video streaming dummy, chat

--- service_chat/cmd/main.go
+++ service_chat/cmd/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"flag"
 	"github.com/go-chi/chi"
 	"github.com/go-chi/chi/middleware"
 	"github.com/orgs/DonSTUhackathon/beatifullBack/service_chat/handler"
@@ -11,17 +13,36 @@ import (
 func main() {
 	r := chi.NewRouter()
 	r.Use(middleware.RedirectSlashes)
-
+	addr := flag.String("addr", ":8080", "http service address")
+	var h handler.Adapter
+	var err error
+	h.Db, err = sql.Open("postgres", "user=admin password=4321 dbname=oggetto_db sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer h.Db.Close()
 	/////////////////////////////////////////////////////////////////////////////////////////////
-	r.Route("/chat/{chat_id}/", func(r chi.Router) {
-		r.Get("/", handler.AuthMiddleware(http.HandlerFunc(handler.SocketHandler)))
-		// r.Post("/sendmessage", handler.AuthMiddleware())
-		// r.Post("/deletemessage", handler.AuthMiddleware())
-		// r.Post("/editmessage", handler.AuthMiddleware())
-		// r.Post("/closesocket", handler.AuthMiddleware())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer h.Db.Close()
+
+	r.Route("/api", func(r chi.Router) {
+		r.Get("/users", h.GetUsers)
+		r.Get("/chats", h.GetChats)
+		r.Route("/messages", func(r chi.Router) {
+			r.Get("/", h.GetMessages)
+			r.Post("/send", h.CreateMessage)
+			r.Post("/delete", h.DeleteMessage)
+			r.Post("/edit", h.UpdateMessage)
+		})
 	})
 
-	http.HandleFunc("/start", handler.SocketHandler)
-	log.Fatal(http.ListenAndServe("localhost:8080", nil))
+	go h.HandleMessages()
 
+	err = http.ListenAndServe(*addr, r)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
--- service_chat/handler/handler.go
+++ service_chat/handler/handler.go
@@ -1,135 +1,221 @@
 package handler
 
 import (
-	"bufio"
+	"database/sql"
+	"encoding/json"
+	"github.com/gorilla/mux"
 	"github.com/gorilla/websocket"
-	// "golang.org/x/oauth2"
-	// "golang.org/x/oauth2/google"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
-	"os"
-	"strings"
-	"time"
 )
 
-var upgrader = websocket.Upgrader{}
-var connections = []*websocket.Conn{}
-
-// var oauth2Config = &oauth2.Config{
-// 	ClientID:     "",
-// 	ClientSecret: "",
-// 	RedirectURL:  "",
-// 	Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email"},
-// 	Endpoint:     google.Endpoint,
-// }
-
-func AuthMiddleware(next http.Handler) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		//if r.token == expected token
-		next.ServeHTTP(w, r)
-	})
+type Adapter struct {
+	Db        *sql.DB
+	Clients   map[*websocket.Conn]bool
+	Broadcast chan Message
+	UPG       websocket.Upgrader
 }
 
-func removeConn(slice []*websocket.Conn, val *websocket.Conn) []*websocket.Conn {
-	index := -1
-	for i, v := range slice {
-		if v == val {
-			index = i
-			break
-		}
+func (a Adapter) NewAdapter() *Adapter {
+	a.UPG = websocket.Upgrader{
+		CheckOrigin: func(r *http.Request) bool {
+			return true
+		},
 	}
-	if index != -1 {
-		if index < len(slice)-1 {
-			copy(slice[index:], slice[index+1:])
-		}
-		slice = slice[:len(slice)-1]
-	}
-	return slice
+	a.Clients = make(map[*websocket.Conn]bool)
+	a.Broadcast = make(chan Message)
+	return &a
 }
 
-func SocketHandler(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
+type User struct {
+	ID        int    `json:"id"`
+	GoogleID  string `json:"google_id"`
+	Name      string `json:"name"`
+	Birthdate string `json:"birthdate"`
+	Position  string `json:"position"`
+}
+
+type Chat struct {
+	ID1 int `json:"id1"`
+	ID2 int `json:"id2"`
+}
+
+type Message struct {
+	ID       int    `json:"id"`
+	ChatID   int    `json:"chat_id"`
+	UserID   int    `json:"user_id"`
+	Content  string `json:"content"`
+	IsHidden bool   `json:"is_hidden"`
+}
+
+func (a Adapter) GetUsers(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var users []User
+
+	rows, err := a.Db.Query("SELECT id, google_id, name, birthdate, position FROM users")
 	if err != nil {
-		log.Print("Error during connection upgradation:", err)
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var user User
+		err = rows.Scan(&user.ID, &user.GoogleID, &user.Name, &user.Birthdate, &user.Position)
+		if err != nil {
+			log.Fatal(err)
+		}
+		users = append(users, user)
+	}
+
+	json.NewEncoder(w).Encode(users)
+}
+
+func (a Adapter) GetChats(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var chats []Chat
+
+	rows, err := a.Db.Query("SELECT id1, id2 FROM chats")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var chat Chat
+		err = rows.Scan(&chat.ID1, &chat.ID2)
+		if err != nil {
+			log.Fatal(err)
+		}
+		chats = append(chats, chat)
+	}
+
+	json.NewEncoder(w).Encode(chats)
+}
+
+func (a Adapter) GetMessages(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var messages []Message
+
+	rows, err := a.Db.Query("SELECT id, chat_id, user_id, content, ishidden FROM messages")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var message Message
+		err = rows.Scan(&message.ID, &message.ChatID, &message.UserID, &message.Content, &message.IsHidden)
+		if err != nil {
+			log.Fatal(err)
+		}
+		messages = append(messages, message)
+	}
+
+	json.NewEncoder(w).Encode(messages)
+}
+
+func (a Adapter) CreateMessage(w http.ResponseWriter, r *http.Request) {
+	var message Message
+
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
-	defer conn.Close()
 
-	connections = append(connections, conn)
+	sqlStatement := `INSERT INTO messages (chat_id, user_id, content, ishidden) VALUES ($1, $2, $3, $4) RETURNING id`
+	err = a.Db.QueryRow(sqlStatement, message.ChatID, message.UserID, message.Content, message.IsHidden).Scan(&message.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+
+	a.Broadcast <- message
+}
+
+func (a Adapter) UpdateMessage(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	var message Message
+
+	err := json.NewDecoder(r.Body).Decode(&message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sqlStatement := `UPDATE messages SET chat_id=$1, user_id=$2, content=$3, ishidden=$4 WHERE id=$5 RETURNING id`
+	err = a.Db.QueryRow(sqlStatement, message.ChatID, message.UserID, message.Content, message.IsHidden, params["id"]).Scan(&message.ID)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(message)
+}
+
+func (a Adapter) DeleteMessage(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+
+	sqlStatement := `DELETE FROM messages WHERE id = $1`
+	res, err := a.Db.Exec(sqlStatement, params["id"])
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	count, err := res.RowsAffected()
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if count == 0 {
+		http.Error(w, "Message not found", http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (a Adapter) HandleConnections(w http.ResponseWriter, r *http.Request) {
+	ws, err := a.UPG.Upgrade(w, r, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ws.Close()
+
+	a.Clients[ws] = true
 
 	for {
-		messageType, message, err := conn.ReadMessage()
+		var msg Message
+		err := ws.ReadJSON(&msg)
 		if err != nil {
-			connections = removeConn(connections, conn)
-			log.Println("Error during message reading:", err)
+			delete(a.Clients, ws)
 			break
 		}
-		//work with msg
-		log.Printf("Server: %s", message)
-		for _, c := range connections {
-			if c != conn {
-				err = c.WriteMessage(messageType, message[:len(message)-1])
-				if err != nil {
-					connections = removeConn(connections, conn)
-					log.Println("Error during message writing:", err)
-					break
-				}
-			}
-		}
+
+		a.Broadcast <- msg
 	}
-	connections = removeConn(connections, conn)
 }
 
-func userInput(inputChan chan string) {
-	reader := bufio.NewReader(os.Stdin)
-	msg, _ := reader.ReadString('\n')
-	inputChan <- msg
-}
-
-func chatInputHandler(conn *websocket.Conn, done chan interface{}) {
+func (a Adapter) HandleMessages() {
 	for {
-		inputChan := make(chan string)
-		go userInput(inputChan)
-		select {
-		case input := <-inputChan:
-			if strings.TrimSpace(input) == "" {
-				continue
-			}
-			err := conn.WriteMessage(websocket.TextMessage, []byte(input))
+		msg := <-a.Broadcast
+		for client := range a.Clients {
+			err := client.WriteJSON(msg)
 			if err != nil {
-				log.Println("Error during writing to websocket:", err)
-				return
+				log.Printf("Error: %v", err)
+				client.Close()
+				delete(a.Clients, client)
 			}
-
-			select {
-			case <-done:
-				log.Println("Receiver Channel Closed")
-			case <-time.After(time.Duration(1) * time.Second):
-				log.Println("Timeout in closing receiving channel")
-			}
-			return
-		}
-	}
-}
-func SendMessage(w http.ResponseWriter, r *http.Request) {
-	conn, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("Error during connection upgradation:", err)
-		return
-	}
-	defer conn.Close()
-
-	messageType, message, err := conn.ReadMessage()
-	if err != nil {
-		log.Println("Error during message reading:", err)
-		return
-	}
-
-	for _, c := range connections {
-		err = c.WriteMessage(messageType, message)
-		if err != nil {
-			log.Println("Error during message writing:", err)
-			return
 		}
 	}
 }
